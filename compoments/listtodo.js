ab6aab4b5ca41fc784a1dfdc8d57fe2f35b088c8
quiltacv <quilt@atmarkcafe.org>
2018-05-21 23:47:26 +0700

    add redux

--- compoments/listtodo.js
+++ compoments/listtodo.js
@@ -12,49 +12,19 @@ class ListTodo extends Component {
     var ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 != r2 });
     this.state = {
       text: this.props.myName,
-      mang: this.props.data,
-      dataSource: ds.cloneWithRows(this.props.data)
     };
   }
-  componentDidMount(){
-    setInterval(() => {
-            this.setState({
-                mang: this.props.data,
-                dataSource: this.state.dataSource.cloneWithRows(this.props.data) });
-        }, 10);
-    self = this
-  }
-
-  // save = async()=>{
-  //   try {
-  //     await AsyncStorage.setItem('@Array:key', JSON.stringify(this.state.mang));
-  //     console.log('SAVE!');
-  //     this.get()
-  //   } catch (e) {
-  //     console.log(e);
-  //   }
-  // }
-  //
-  // get = async()=>{
-  //   try {
-  //     var mang = await AsyncStorage.getItem('@Array:key');
-  //     mang = JSON.parse(mang);
-  //     this.setState({
-  //       mang: mang,
-  //       dataSource: this.state.dataSource.cloneWithRows(mang)
-  //     })
-  //   } catch (e) {
-  //     console.log(e);
-  //   }
-  // }
   render() {
+    console.log("---Render---");
+    console.log(this.props.dataSource);
     return(
         <ListView
           enableEmptySections = { true }
-          dataSource = { this.state.dataSource }
+          dataSource = { this.props.dataSource }
           renderRow= { this.renderRow.bind(this) }
           renderHeader = { this.renderAdd.bind(this) }
-        />
+        >
+        </ListView>
     )
   }
 
@@ -62,7 +32,7 @@ class ListTodo extends Component {
     return (
       <View style={ styles.container }>
         <TextInput
-          value = { this.state.text }
+          value = { this.props.myName }
           style={ styles.input }
           placeholder="Search..."
           onChangeText={ (text) => this.setState({ text: text }) }
@@ -76,8 +46,7 @@ class ListTodo extends Component {
 
   addCell() {
     this.props.dispatch({type: 'ADD_DATA', text: this.state.text })
-
-}
+  }
   todoEdit(dataRow,index){
     this.props.navigator.push({
       component: TodoEdit,
@@ -99,7 +68,7 @@ class ListTodo extends Component {
       else {
           current = 'true'
       }
-      mang = self.props.data
+      mang = this.props.data
       mang[rowID] = [dataRow[0], current]
       this.props.dispatch({type: 'CHECKED', index: rowID, value: dataRow[0], status: current })
     }
@@ -134,7 +103,8 @@ class ListTodo extends Component {
 const mapStateToProps = (state) => {
   return {
     myName: state.name,
-    data: state.mang
+    data: state.mang,
+    dataSource: state.dataSource.cloneWithRows(state.mang)
   };
 }
 
--- index.js
+++ index.js
@@ -1,35 +1,50 @@
-import { AppRegistry, AppState, AsyncStorage, Text } from 'react-native';
+import { AppRegistry, AppState, AsyncStorage, Text, ListView } from 'react-native';
 import App from './App';
 import { createStore } from 'redux';
 import { Provider } from 'react-redux';
 import React, { Component } from 'react';
-
+var ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 === r2 });
+var mang = [['acb','true']
+]
 const defaultState = {
   name: 'fox zi',
   value: 0,
-  mang: [],
+  mang: mang,
+  dataSource: ds.cloneWithRows(mang)
 }
 
 const reducer = (state = defaultState, action) => {
+  let newState = Object.assign({}, state);
   switch (action.type) {
     case 'ADD_DATA':
       return {
         mang: [
           ...state.mang,
           [action.text, 'false']
-        ]
+        ],
+        dataSource: state.dataSource.cloneWithRows(state.mang)
       }
     case 'DELETE_DATA':
-      state.mang.splice(action.index, 1)
-      return { ...state }
+      newState.mang.splice(action.index, 1);
+      return {
+        mang: newState.mang,
+        dataSource: newState.dataSource.cloneWithRows(newState.mang)
+      }
 
     case 'EDIT_DATA':
-      state.mang[action.index] = [action.value, action.status];
-      return state
+      newState.mang[action.index] = [action.value, action.status];
+      return {
+        mang: newState.mang,
+        dataSource: newState.dataSource.cloneWithRows(newState.mang)
+      };
 
     case 'CHECKED':
-      state.mang[action.index] = [action.value, action.status];
-      return state
+      console.log(state.dataSource);
+      newState.mang[action.index] = [action.value, action.status];
+      return {
+        mang: newState.mang,
+        dataSource: newState.dataSource.cloneWithRows(newState.mang)
+      };
 
     case 'GET_DATA':
       state.mang = action.data
